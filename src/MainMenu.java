f304c8e1b19c2c58387c71f08e01d2e27dc2af4b
Struan McKenzie <2686829@dundee.ac.uk>
2026-04-14 15:06:46 +0100

    add gui main menu class

---
+++ src/MainMenu.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainMenu {
+    /**
+     * Constructor
+     *
+     *
+     *
+     *
+     */
+    public MainMenu() {
+
+        launchUI();
+    }
+
+    private void launchUI() {
+        JFrame main = new JFrame("Project Nova");
+
+
+    }
+}
